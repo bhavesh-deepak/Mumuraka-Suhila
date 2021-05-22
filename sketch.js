d7b3adba22d1823f5d1efb031cc7647f51e07b91
bhavesh-deepak <76872051+bhavesh-deepak@users.noreply.github.com>
2021-05-22 12:28:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(176);
+   fill("yellow");
+  textSize(17)
+  text("Mumuraka Suhila",250,50);
+  
+  fill("yellow");
+  textSize(17)
+  text("_______________",250,50);
+  
+  
+  fill("yellow");
+  textSize(17)
+  text("Onion - 1",250,90);
+  
+  fill("yellow");
+  textSize(17)
+  text("Tomato - 1",250,120);
+  
+  fill("yellow");
+  textSize(17)
+  text(" adarak - 1",250,150);
+  
+  fill("yellow");
+  textSize(17)
+  text(" Lasun - 5",250,180);
+  
+  fill("yellow");
+  textSize(17)
+  text(" Oil - 2litre",250,210);
+  
+  fill("yellow");
+  textSize(17)
+  text("Mumura - 500kg",250,240);
+  
+  fill("yellow");
+  textSize(17)
+  text("water-1/2",250,270);
+  
+  fill("yellow");
+  textSize(17)
+  text("Namake - taste  ",250,300);
+  
+  
+  
+}
